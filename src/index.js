bfbd54d5375d8fb659434ee9b8f161ade6d7dc7c
Bob Krstic <slobodankrsticnis@yahoo.com>
2019-12-27 16:06:01 -0800

    test push

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+// what do i want to render and where do i want to render it?
 ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
